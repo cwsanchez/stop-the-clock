9896d61df2cbc8791c2627ca9ed67c86892397ef
csan <blaxicrish@gmail.com>
2023-06-20 21:36:51 +0000

    Added input checking fucntion for username state.

--- js-react/src/components/App/App.js
+++ js-react/src/components/App/App.js
@@ -7,6 +7,27 @@ function App() {
 
   const [time, setTime] = React.useState(0);
   const [running, setRunning] = React.useState(false);
+  const [username, setUsername] = React.useState('');
+
+  const randomList = [ 'buddy', 'pal', 'bro' ];
+
+  const isStringValid = (str) => {
+    const pattern = /^[a-zA-Z_\-]+$/;
+
+    return pattern.test(str)
+  }
+
+  const submitUsername = (username) => {
+   if ( !(isStringValid(username)) ) {
+    return "Only A-Z, a-z, -, and _ characters are permitted!"
+   }
+   else if ( !(randomList.includes(username)) ) { 
+    return "Username already exists!"
+   }
+   else {
+    setUsername(username); 
+   }
+  }
 
   return (
     <div className="App">
